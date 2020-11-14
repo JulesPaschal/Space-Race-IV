115f5e7b699c25ca4ebf163ccbc0c5ed7e2df0a3
sam ms <samuel.mayer_svarczkopf@tufts.edu>
2020-11-13 20:59:02 -0500

    Added janky look/movement script to new scene
    I moved a package i made of a prefab cube with a camera, character controller, and several scripts for walking and moving. They were running but not how I want them to before I imported them, but now that I've moved them into RocketGameIV they are just not working at all. If any of yall wanna take a look be my guest, otherwise I will get it fixed in TA hours Saturday.

---
+++ RocketsGameIV/Assets/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    
+    void Update()
+    {
+        float X = Input.GetAxis("Horizontal");
+        float Z = Input.GetAxis("Vertical");
+
+        //this is supposed to allow movement local to the player object so that
+        //turning will adjust the absolute direction that pressing W would take
+        //you, however it is not doing that
+        Vector3 move = transform.right * X + transform.forward * Z;
+        controller.Move(move * speed * Time.deltaTime);
+
+    }
+}
---
+++ RocketsGameIV/Assets/mouseLook.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseLook : MonoBehaviour
+{
+    //float to modify mouse sensitivity
+    public float mouseSensitivity = 100f;
+    //Transform to link player 3D object to mouseLook script
+    public Transform playerBodyUp;
+    public Transform playerBodyRight;
+    //float to track rotation of camera around X axis
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    void Start()
+    {
+        //hide the cursor and keep it in the center of the screen
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        //take input from mouse movement on X and Y axes, multiply by
+        //sensitivity and tie it to deltaTime instead of framerate
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //camera rotation around the x axis is controlled by the mouse,
+        //subtracted from the default rotation which is 0
+        xRotation -= mouseY;
+        yRotation += mouseX;
+
+        //rotates the camera around the x axis independent of the player object
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+
+        playerBodyRight.Rotate(Vector3.left * mouseY);
+        //rotates the camera and player object together around the y axis
+        playerBodyUp.Rotate(Vector3.up * mouseX);
+
+    }
+}
