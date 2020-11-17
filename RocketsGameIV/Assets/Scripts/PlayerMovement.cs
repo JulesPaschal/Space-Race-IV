9f1315f34f56f6df205b557a1576ab4d3418155f
juliathehobbit <60908588+juliathehobbit@users.noreply.github.com>
2020-11-17 16:07:56 -0500

    fuck

--- RocketsGameIV/Assets/Scripts/PlayerMovement.cs
+++ RocketsGameIV/Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 
@@ -9,17 +11,38 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 12f;
-    
+
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        speed = 0;
+    }
+
+
+   
     void Update()
     {
-        float X = UnityEngine.Input.GetAxis("Horizontal");
-        float Z = UnityEngine.Input.GetAxis("Vertical");
+        if (UnityEngine.Input.inputString == "Space"){
+            float X = UnityEngine.Input.GetAxis("Horizontal");
+            float Z = UnityEngine.Input.GetAxis("Vertical");
 
-        //this is supposed to allow movement local to the player object so that
-        //turning will adjust the absolute direction that pressing W would take
-        //you, however it is not doing that
-        Vector3 move = transform.right * X + transform.forward * Z;
-        controller.Move(move * speed * Time.deltaTime);
+            //this is supposed to allow movement local to the player object so that
+            //turning will adjust the absolute direction that pressing W would take
+            //you, however it is not doing that
+            Vector3 move = transform.right * X + transform.forward * Z;
+            controller.Move(move * speed * Time.deltaTime);
+        }
 
+    }
+
+     void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(-1.0f, movementY, movementX);
+        rb.AddForce(movement * speed);
     }
 }
