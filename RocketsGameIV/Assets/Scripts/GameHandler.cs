b1aa1e99a6d8c06ba4a7eab54bf292a9d4ca78dd
GregBurrill <GregBurrillWeb@gmail.com>
2020-12-08 14:37:33 -0500

    Adjusted spacing in Menu screen, added GameHandler for Stats and renamed scenes to fit

---
+++ RocketsGameIV/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameHandler : MonoBehaviour
+{
+
+	public static int racePlace;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKey("escape")) {
+			Application.Quit ();
+		}
+    }
+		
+
+	public void Start1(){
+		SceneManager.LoadScene("Level1");
+	}
+
+	public void Start2(){
+		SceneManager.LoadScene("Level1");
+	}
+
+	public void Start3(){
+		SceneManager.LoadScene("Level1");
+	}
+
+
+	public void WinScreen(){
+		SceneManager.LoadScene("EndWin");
+	}
+		
+	public void Restart(){
+		SceneManager.LoadScene("MainMenu");
+
+	}
+
+	public void Credits(){
+		SceneManager.LoadScene("CRedits");
+	}
+
+	public void Quit(){
+		Application.Quit();
+	}
+
+
+}
