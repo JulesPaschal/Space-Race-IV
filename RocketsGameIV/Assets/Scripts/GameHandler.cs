cf295335a45a92f80a80415a7177886a5a68b618
sam ms <samuel.mayer_svarczkopf@tufts.edu>
2020-12-15 17:54:07 -0500

    final build 12/15

--- RocketsGameIV/Assets/Scripts/GameHandler.cs
+++ RocketsGameIV/Assets/Scripts/GameHandler.cs
@@ -18,9 +18,9 @@ public class GameHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		if (Input.GetKey("escape")) {
-			Application.Quit ();
-		}
+		//if (Input.GetKey("escape")) {
+		//	Application.Quit ();
+		//}
     }
 		
 
