c59af91dee0c32ff07838eb2331bf534fda02fbc
juliathehobbit <60908588+juliathehobbit@users.noreply.github.com>
2020-12-22 12:27:01 -0500

    added win/lose stuff to level 1

---
+++ RocketsGameIV/Assets/Scripts/WinLoseButtons.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class WinLoseButtons : MonoBehaviour
+{
+	public GameObject WinUI;
+	public GameObject LoseUI;
+	public int CurrentLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+    	WinUI.SetActive(false);
+    	LoseUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextRace(){
+    	if (CurrentLevel == 1){
+    		SceneManager.LoadScene("Level2");
+    	}
+    	if(CurrentLevel == 2){
+    		SceneManager.LoadScene("Level3");
+    	}
+    	if (CurrentLevel == 3){
+    		SceneManager.LoadScene("Credits");
+    	}
+    }
+}
